934df4c7b712e3993a14161fea51fc31bf3b8730
Japneet Singh <singhjapneetssa@gmail.com>
2020-06-30 03:31:35 +0530

    Configured react-i18next and added fallbacks for loading period
    resolves #12

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+
 
 import ChoiceBoard from './routes/ChoiceBoard';
 
@@ -13,7 +14,9 @@ function App() {
       <Router>
         <Switch>
           <Route path="/">
+          <Suspense fallback="loading">
             <ChoiceBoard />
+          </Suspense>
           </Route>
         </Switch>
       </Router>
