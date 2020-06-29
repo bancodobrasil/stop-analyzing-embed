09d9f71342fc71a458cc84ec01b34930e0e068cd
RafaelMM <rafamarts@gmail.com>
2020-06-29 14:52:30 -0300

    #4 Create drop card area for LikeMode

--- app/src/routes/LikeBoard.tsx
+++ app/src/routes/LikeBoard.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import LikeCard from '../components/LikeCard';
+import DropCardArea from '../components/DropCardArea';
 
 
 function LikeBoard() {
 
+    const dislikeClick = (e, card) => {
+        console.log("Dislike button clicked!!")
+        console.log(card)
+    }
+
+    const likeClick = (e, card) => {
+        console.log("Like button clicked!!")
+        console.log(card)
+    }
+
     return (
         <div className="row text-center">
-            <div className="col-lg-12 mb-4">
+            <DropCardArea onDrop={dislikeClick} className="dropDislikeArea" />
+            <div className="board">
                 <LikeCard
                     id="X"
                     title="Like Mode Choice"
                     paragraph="That's a good choice!"
                     imageURL="https://picsum.photos/seed/x1/200/300"
-                    onLikeClick={(e, card) => {
-                        console.log("Like button clicked!!")
-                        console.log(card)
-                    }}
-                    onDeslikeClick={(e, card) => {
-                        console.log("Dislike button clicked!!")
-                        console.log(card)
-                    }}
+                    onLikeClick={likeClick}
+                    onDislikeClick={dislikeClick}
+                    draggable={true}
                 />
             </div>
+            <DropCardArea onDrop={likeClick} className="dropLikeArea"/>
         </div>
     )
 }
