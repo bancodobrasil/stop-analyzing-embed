9b5edaf05d528734d9a8d3ed0eb9d1802cbaeecd
tiagostutz <tiagostutz@gmail.com>
2020-07-21 00:12:25 -0300

    fix: remove unused imports

--- app/src/routes/ChoiceBoard.tsx
+++ app/src/routes/ChoiceBoard.tsx
@@ -1,5 +1,4 @@
-import { type } from 'os';
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
