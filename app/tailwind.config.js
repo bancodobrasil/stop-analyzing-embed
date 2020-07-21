a8f510d6abd024cd70eacbea67e6d160adbf6737
giteshk11 <writemail2gitesh@gmail.com>
2020-07-21 23:38:53 +0530

    initial tailwind setup & configuration. refs #36

---
+++ app/tailwind.config.js
@@ -0,0 +1,49 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('./src/styles/colors');
+
+module.exports = {
+  purge: ['./public/**/*.html', './src/**/*.vue'],
+  theme: {
+    extend: {
+      colors,
+      screens: {
+        xl: '1280px',
+        xxl: '1920px',
+      },
+      spacing: {
+        '0.5': '0.125rem',
+        '1.5': '0.375rem',
+        '14': '3.5rem',
+        '128': '32rem',
+        '160': '40rem',
+      },
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
+      inset: {
+        '1/2': '50%',
+      },
+      zIndex: {
+        '-1': '-1',
+        '100': '100',
+        '110': '110',
+      },
+    },
+  },
+  variants: [
+    'responsive',
+    'group-hover',
+    'group-focus',
+    'focus-within',
+    'first',
+    'last',
+    'odd',
+    'even',
+    'hover',
+    'focus',
+    'active',
+    'visited',
+    'disabled',
+  ],
+  plugins: [],
+};
