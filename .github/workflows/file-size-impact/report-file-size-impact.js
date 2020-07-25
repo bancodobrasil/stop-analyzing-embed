67ddcb0878494176972d4f02cc8fe65c193fe54e
tiagostutz <tiagostutz@gmail.com>
2020-07-24 22:58:45 -0300

    feat: organized workflow experimental scripting

---
+++ .github/workflows/file-size-impact/report-file-size-impact.js
@@ -0,0 +1,12 @@
+const { reportFileSizeImpact, readGithubWorkflowEnv } = require("@jsenv/file-size-impact");
+
+reportFileSizeImpact({
+  ...readGithubWorkflowEnv(),
+  buildCommand: "npm run build --prefix ../../../app",
+  trackingConfig: {
+    "app/build": {
+      "../../../app/build/**/*": true,
+      "../../../app/build/**/*.map": false,
+    },
+  },
+});
