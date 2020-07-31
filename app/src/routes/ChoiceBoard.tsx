e52a99c8130a92ea460e1cf358efd0c18adaf8cf
Ukpai Ugochi <62933980+hannydevelop@users.noreply.github.com>
2020-07-31 06:45:48 +0100

    Update ChoiceBoard.tsx

--- app/src/routes/ChoiceBoard.tsx
+++ app/src/routes/ChoiceBoard.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import image1 from '../assets/images/andrew-johnson-ULKR-8CtHmM-unsplash.jpg';
 import image2 from '../assets/images/providence-doucet-FjwtL3YSZ9U-unsplash.jpg';
 import ChoiceCard from '../components/ChoiceCard';
-import TimerComponent from '../components/TimerComponent/TimerComponent';
+
 import useChoiceBoardModel from './useChoiceBoardModel';
 
 const H3 = styled.h3`
@@ -32,7 +32,7 @@ function ChoiceBoard() {
             <div className="row text-center">
               <div className="col-lg-12 m-4">
                 <H3>{t('Which one would you like to choose?')} </H3>
-                <TimerComponent timeout={15} />
+    
               </div>
             </div>
             <div className="row text-center">
