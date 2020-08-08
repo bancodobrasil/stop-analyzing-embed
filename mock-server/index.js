546fc954f4ee82764f215297eca61db1c978172d
tiagostutz <tiagostutz@gmail.com>
2020-08-08 09:30:21 -0300

    feat: add serverPortNumber as variable and with 4000 as value to avoid collision. refs #58

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mockApi = require('./API/mockApi');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mockApi = require("./API/mockApi");
 
 var app = express();
 app.use(bodyParser.json());
-
-
-
-app.listen(3000, () => console.log('server started successfully at port : 3000....'));
-app.use('/', mockApi);
+const serverPort = 4000;
+app.listen(serverPort, () => console.log(`server started successfully at port ${serverPort}....`));
+app.use("/", mockApi);
