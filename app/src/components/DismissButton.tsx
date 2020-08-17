74b8510f9a9e116475dc55e624126db2d94afb99
TCashion <58757351+TCashion@users.noreply.github.com>
2020-08-17 16:29:05 -0600

    add type to button

--- app/src/components/DismissButton.tsx
+++ app/src/components/DismissButton.tsx
@@ -11,6 +11,7 @@ function DismissButton({ buttonLabel, onClick }: DismissButtonProps) {
       <button
         className="text-orange-200 rounded-lg border-2 border-red-700 p-2 border-opacity-50 bg-red-700 bg-opacity-25 shadow"
         onClick={onClick}
+        type="button"
       >
         {buttonLabel}
       </button>
--- app/src/routes/ChoiceBoard.tsx
+++ app/src/routes/ChoiceBoard.tsx
@@ -1,4 +1,4 @@
-import anime from 'animejs/lib/anime.es.js';
+import anime from 'animejs/lib/anime';
 import React, { MouseEvent, Suspense, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
