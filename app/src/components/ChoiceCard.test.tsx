642d6cf1090624dc98a54de7e6ce0994c85524d3
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-09-23 22:56:35 -0500

    fix: repaired tests in ChoiceCard.test.tsx

--- app/src/components/ChoiceCard.test.tsx
+++ app/src/components/ChoiceCard.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 
-import { findAllByAltText, findByAltText, getByAltText, queryByAltText, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 
 import ChoiceCard from './ChoiceCard';
@@ -18,7 +18,12 @@ it('should render: title. Should not render: paragraph, image', () => {
 it('should render: title and image. Should not render: paragraph', () => {
   const imgURLTest = 'http://image';
   const { queryByText, queryByAltText } = render(
-    <ChoiceCard id="X" title="Title" imageURL={imgURLTest} imageALT={imgURLTest} />
+    <ChoiceCard
+      id="X"
+      title="Title"
+      imageURL={imgURLTest}
+      imageALT={imgURLTest}
+    />
   );
   expect(queryByText('Title')).toBeInTheDocument();
   expect(queryByText('Paragraph')).toBeNull();
