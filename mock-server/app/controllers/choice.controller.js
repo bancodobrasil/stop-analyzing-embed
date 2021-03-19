5359de12de7905f3fff65ad0b35c877ef9ab2d64
Dythic <quentin.henry@epitech.eu>
2021-03-19 17:30:19 +0100

    [add] choice route post

--- mock-server/app/controllers/choice.controller.js
+++ mock-server/app/controllers/choice.controller.js
@@ -1,12 +1,24 @@
 const db = require("../../db.json");
+const Choices = require("../models/choice.model.js");
 
 const findAll = async(req, res) => {
     return res.json(db);
 };
 
 const addOne = async(req, res) => {
-    const choice = req.body;
-    return res.json(db);
+    await Choices.sync();
+
+    const newChoice = await Choices.build({
+        id_choise: req.body.id_choise,
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        contentURL: req.body.contentURL,
+        size: req.body.size,
+        color: req.body.color,
+    });
+    
+    newChoice.save();
+    return res.json({message: "Send"});
 };
 
 module.exports = {findAll, addOne}
